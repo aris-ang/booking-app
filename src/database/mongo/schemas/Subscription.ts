30795d74e6ae1bfc68e9bc3b16a5b35ef1a075c7
aris-ang <mrar0sgr@gmail.com>
2024-02-18 18:54:14 +0200

    #1: Updated the way Service, User are referenced in Subscription schema

--- src/database/mongo/schemas/Subscription.ts
+++ src/database/mongo/schemas/Subscription.ts
@@ -1,17 +1,27 @@
 import {model, Schema, Types} from "mongoose";
+import {User} from "./User";
+import {Service} from "./Service";
 
 interface ISubscription {
     title: string,
-    service: Types.ObjectId,
-    user: Types.ObjectId,
+    service: Types.ObjectId | Record<string, unknown>,
+    user: Types.ObjectId | Record<string, unknown>,
     activatedAt: Date,
     expiresAt: Date
 }
 
 const subscriptionSchema = new Schema<ISubscription>({
     title: {type: String, required:true},
-    service: {type: Types.ObjectId, required: true},
-    user: {type: Types.ObjectId, required: true},
+    service: {
+        type: Types.ObjectId,
+        required: true,
+        ref: Service
+    },
+    user: {
+        type: Types.ObjectId,
+        required: true,
+        ref: User
+    },
     activatedAt: {type: Date, required: true},
     expiresAt: {type: Date, required: true}
 });
