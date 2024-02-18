c3eef2606e4b55ed02efe7531b217017137a2d4b
aris-ang <mrar0sgr@gmail.com>
2024-02-18 18:53:24 +0200

    #1: Updated the way Service, User and Subscription are referenced in Booking schema

--- src/database/mongo/schemas/Booking.ts
+++ src/database/mongo/schemas/Booking.ts
@@ -1,17 +1,34 @@
 import {model, Schema, Types} from "mongoose";
+import {Service} from "./Service";
+import {User} from "./User";
+import {Subscription} from "./Subscription";
 
 interface IBooking {
-    service: Types.ObjectId,
-    user: Types.ObjectId,
-    subscription: Types.ObjectId,
-    timestamp: Date
+    service: Types.ObjectId | Record<string, unknown>,
+    user: Types.ObjectId | Record<string, unknown>,
+    subscription: Types.ObjectId | Record<string, unknown>,
+    timestamp: Date,
+    createdAt:Date
 }
 
 const bookingSchema = new Schema<IBooking>({
-    service: {type: Types.ObjectId, required: true},
-    user: {type: Types.ObjectId, required: true},
-    subscription: {type: Types.ObjectId, required: true},
-    timestamp: {type: Date, required: true}
+    service: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: Service
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: User
+    },
+    subscription: {
+        type: Schema.Types.ObjectId,
+        required: false,
+        ref: Subscription
+    },
+    timestamp: {type: Date, required: true},
+    createdAt: {type: Date, required: true}
 });
 
 export const Booking = model<IBooking>('Booking', bookingSchema)
