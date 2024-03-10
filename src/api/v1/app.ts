3a9885146907ed97ee938875bb95e5846077e232
aris-ang <mrar0sgr@gmail.com>
2024-03-10 17:34:10 +0200

    #11: Include body-parser.

--- src/api/v1/app.ts
+++ src/api/v1/app.ts
@@ -1,11 +1,14 @@
+import path from "path";
 import express, {Express} from "express";
 import swaggerUI from "swagger-ui-express";
-
+import bodyParser from "body-parser";
 import {router as usersRouter} from "../v1/routes/users";
 import swaggerDocument from "./docs/swagger.json";
 
 const app: Express = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/book", express.static(path.join(path.dirname(__filename), "public")));
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
